a32f169555fb7b3ce8d70a10e766f20d44841d84
youneshajji <younes.hajji@prosesspilotene.no>
2021-03-19 15:59:41 +0100

    test

--- src/components/Security/ApiEndpoints.tsx
+++ src/components/Security/ApiEndpoints.tsx
@@ -1,4 +1,4 @@
 export const webApi = "https://app-gyld-webapi-dev.azurewebsites.net/api"
-// export const identityWebApi = "https://app-gyld-identitywebapi-dev.azurewebsites.net/api"
+export const identityWebApi = "https://app-gyld-identitywebapi-dev.azurewebsites.net/api"
 
-export const identityWebApi = "https://localhost:44385/api"
+// export const identityWebApi = "https://localhost:44385/api"
